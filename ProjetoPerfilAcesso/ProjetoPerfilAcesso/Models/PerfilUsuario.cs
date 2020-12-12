8b94fa4ba3190f79af8cf44cd015a3744ea64ad5
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2020-12-12 09:06:58 -0300

    Criação das classes e das tabelas atraves do migration

---
+++ ProjetoPerfilAcesso/ProjetoPerfilAcesso/Models/PerfilUsuario.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoPerfilAcesso.Models
+{    
+    public class PerfilUsuario
+    {
+        [Display(Name = "Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "TipoUsuario")]
+        [ForeignKey("TipoUsuario")]
+        [Column(Order = 1)]
+        public int IdTipoUsuario { get; set; }
+
+        public virtual TipoUsuario TipoUsuario { get; set; }
+
+
+        //Não precisa criar Usuario na minha Model o Identity ja faz isso. Só preciso referenciar aqui
+        [Display(Name = "Usuário")]
+        [ForeignKey("IdentityUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        public virtual IdentityUser IdentityUser { get; set; }
+
+
+
+    }
+}
